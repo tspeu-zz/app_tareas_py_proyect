e589d3bf626a607d8083cc543d267314b8940a87
jm_b <jmbanchero@gmail.coom>
2020-10-07 18:29:51 +0100

    add login ok

--- tareas/urls.py
+++ tareas/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("update/<pk>/", views.update_task, name="update_task"),
     path('register/', views.register_page, name='register'),
     path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 
 ]
 
--- tareas/views.py
+++ tareas/views.py
@@ -4,9 +4,12 @@ from django.contrib import messages
 from .forms import TaskForm, CreateUserForm
 from .models import Tareas
 # register django default
-# from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+# para retringir el usuario al acceso a las paginas
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# retringir el accseso si el ususaio no está login se redirige a login
+@login_required(login_url='login')
 def lista_tareas(request):
     form = TaskForm()
     if request.method == 'POST':
@@ -33,7 +36,8 @@ def index(request):
     print(_tareas)
     return render(request, "index.html", {"task_form": form, "tareas": _tareas})
 
-
+#
+@login_required(login_url='login')
 def update_task(request, pk):
     task = Tareas.objects.get(id=pk)
     form = TaskForm(instance=task)
@@ -45,18 +49,18 @@ def update_task(request, pk):
     return render(request, "update.html", {"task_edit_form": form})
 
 
-# regiter user
-
-
+# register user
 def register_page(request):
-    form = CreateUserForm()
-
-    if request.method == "POST":
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, 'La cuenta se ha creado con éxito! Hola ' + user)
+    if request.user.is_authenticated:
+        return redirect('lista_tareas')
+    else:
+        form = CreateUserForm()
+        if request.method == "POST":
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'La cuenta se ha creado con éxito! Hola ' + user)
             return redirect('login')
 
     context = {'form': form}
@@ -64,6 +68,26 @@ def register_page(request):
 
     # login user
 def login_page(request):
-    context = {}
-    return render(request, 'login.html', context)
+    if request.user.is_authenticated:
+        return redirect('lista_tareas')
+    else:
+        if request.method == 'POST':
+            usern = request.POST.get('username')
+            passw = request.POST.get('password')
+
+            user = authenticate(request, username=usern, password=passw)
+            # validar
+            if user is not None:
+                login(request, user)
+                return redirect('lista_tareas')
+            else:
+                messages.info(request, 'usuario o constraseña incorrecta' )
+
+        context = {}
+        return render(request, 'login.html', context)
+
+#
+def logout_user(request):
+    logout(request)
+    return redirect('login')
 
